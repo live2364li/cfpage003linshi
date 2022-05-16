d6872a24861b648057f18cb0d97b6cab2f59636c
live2364li <90216011+live2364li@users.noreply.github.com>
2022-05-16 13:01:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="https://late-dream-af37.l4535svip.workers.dev";
+        url.hostname="gsdgdfhf.herokuapp.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
